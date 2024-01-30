21ca42c6a4e5f20ca0feee094acc1f4fc1ee80c8
IvanGlaurdic <146755892+IvanGlaurdic@users.noreply.github.com>
2024-01-30 22:34:04 +0100

    update

--- FoodDelivery/src/main/java/com/dostavljaci/FoodDelivery/controller/OrderController.java
+++ FoodDelivery/src/main/java/com/dostavljaci/FoodDelivery/controller/OrderController.java
@@ -1,6 +1,11 @@
 package com.dostavljaci.FoodDelivery.controller;
 
+import com.dostavljaci.FoodDelivery.entity.MenuItem;
+import com.dostavljaci.FoodDelivery.entity.User;
+import com.dostavljaci.FoodDelivery.service.MenuItemService;
 import com.dostavljaci.FoodDelivery.service.OrderService;
+import com.dostavljaci.FoodDelivery.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import lombok.AllArgsConstructor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.dostavljaci.FoodDelivery.service.RestaurantService;
 import com.dostavljaci.FoodDelivery.entity.Restaurant;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -18,11 +24,32 @@ import java.util.UUID;
 public class OrderController {
     public final OrderService orderService;
     private final RestaurantService restaurantService;
+    private final MenuItemService menuItemService;
+    private final UserService userService;
 
     @GetMapping("/{Name}")
-    public String orderPage(@PathVariable String Name, Model model) {
+    public String orderPage(@PathVariable String Name, Model model, HttpSession session) {
+
+        Object sessionUser = session.getAttribute("user");
+        User user;
+
+        if (sessionUser instanceof User userInstance){
+            user = userService.getUserByUsername(userInstance.getUsername());
+        }
+        else {
+            throw new RuntimeException("Cant locate user:" + sessionUser);
+        }
+        model.addAttribute("user",user);
         Restaurant restaurant=restaurantService.getRestaurantByName(Name);
+
         model.addAttribute("restaurant", restaurant);
+
+        List<MenuItem> menu= menuItemService.getMenuByRestaurantId(restaurant.getId());
+        System.out.print(menu);
+        model.addAttribute("menu",menu);
+
         return "order-page";
     }
+
+
 }
--- FoodDelivery/src/main/java/com/dostavljaci/FoodDelivery/service/MenuItemService.java
+++ FoodDelivery/src/main/java/com/dostavljaci/FoodDelivery/service/MenuItemService.java
@@ -1,13 +1,21 @@
 package com.dostavljaci.FoodDelivery.service;
 
+import com.dostavljaci.FoodDelivery.entity.MenuItem;
 import com.dostavljaci.FoodDelivery.repository.MenuItemRepository;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
 @Data
 public class MenuItemService {
     private final MenuItemRepository menuItemRepository;
+
+    public List<MenuItem> getMenuByRestaurantId(UUID id) {
+        return menuItemRepository.getMenuItemsByRestaurantId(id);
+    }
 }
