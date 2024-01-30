a81a9915bec2c9bf57b4fed7c85386f00a3d0b5c
IvanGlaurdic <146755892+IvanGlaurdic@users.noreply.github.com>
2024-01-30 22:15:07 +0100

    update

--- FoodDelivery/src/main/java/com/dostavljaci/FoodDelivery/controller/OrderController.java
+++ FoodDelivery/src/main/java/com/dostavljaci/FoodDelivery/controller/OrderController.java
@@ -2,10 +2,27 @@ package com.dostavljaci.FoodDelivery.controller;
 
 import com.dostavljaci.FoodDelivery.service.OrderService;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.dostavljaci.FoodDelivery.service.RestaurantService;
+import com.dostavljaci.FoodDelivery.entity.Restaurant;
 
-@Controller
+import java.util.UUID;
+
+@RestController
 @AllArgsConstructor
+@RequestMapping("/order")
 public class OrderController {
     public final OrderService orderService;
+    private final RestaurantService restaurantService;
+
+    @GetMapping("/{Name}")
+    public String orderPage(@PathVariable String Name, Model model) {
+        Restaurant restaurant=restaurantService.getRestaurantByName(Name);
+        model.addAttribute("restaurant", restaurant);
+        return "order-page";
+    }
 }
